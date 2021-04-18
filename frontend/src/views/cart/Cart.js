54846ff42c458e5bd7f124b6b4eb954a672ec810
Arigbede Omobolaji <arigbedeomobolaji@gmail.com>
2021-04-18 13:11:35 +0100

    setup cart view

--- frontend/src/views/cart/Cart.js
+++ frontend/src/views/cart/Cart.js
@@ -1,9 +1,17 @@
-import React, {useEffect} from "react"
-import {useDispatch} from "react-redux"
+// jshint ignore:start
+import React, { useEffect } from "react"
+import {useDispatch, useSelector} from "react-redux"
+import { Link } from "react-router-dom"
 import { addToCartAction } from "../../actions/cartActions"
+import CartItem from "../../components/cartItem/CartItem"
+import MessageBox from "../../components/messagebox/MessageBox"
+import "./cart.css"
 
 const Cart = (props) => {
  const dispatch = useDispatch()
+ const cart = useSelector(state => state.cart)
+ const { cartItems } = cart
+ 
  const productId = props.match.params.id
  const qty = props.location.search ? parseInt(props.location.search.split("=")[1], 10) : 1
 
@@ -12,13 +20,40 @@ const Cart = (props) => {
    dispatch(addToCartAction(productId, qty))
   }
  }, [dispatch, productId, qty])
- 
+ const handleProceedToCheckout = () => {
+  props.history.push("/sigin?redirect=shipping")
+ }
  return (
-  <div>
-   <h1>Your Cart</h1>
-   <h3>Cart History</h3>
-   <p>productId: {productId}</p>
-   <p>Qty Added: {qty}</p>
+  <div className="cards">
+   <div className="cart__details">
+    <h2 className="cart__details__title">Shopping Cart</h2>
+    { cartItems.length === 0 ?
+     (
+      <MessageBox>
+       <Link to="/">Let's go Shopping</Link>
+      </MessageBox>
+     ) :
+     (
+      cartItems.map(cartItem => <CartItem key={cartItem.product} {...cartItem} />) )
+    }
+   </div>
+   {
+    cartItems.length > 0 && (
+     <div className="cart__checkout">
+    <div className="checkout__subtotal">
+    Subtotal ({cartItems.reduce((acc, current) => acc + current.qty, 0)} items) : &nbsp;
+    $ {cartItems.reduce((acc, current) => acc + current.price * current.qty, 0 )}
+    </div>
+    <button
+     className="button button--block button--primary"
+     type="button"
+     onClick={handleProceedToCheckout}
+     disabled={cartItems.length === 0}
+    >Proceed to Checkout</button>
+   </div>
+    )
+   }
+   
   </div>
  )
 }
