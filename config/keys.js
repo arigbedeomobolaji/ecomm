69a6382eec1531943365c75beda0dfc129233491
Arigbede Omobolaji <arigbedeomobolaji@gmail.com>
2021-10-14 17:38:42 +0100

    modify config directory

---
+++ config/keys.js
@@ -0,0 +1,11 @@
+import prod from './prod.js';
+import dev from './dev.js';
+
+let keys = dev;
+if (process.env.NODE_ENV === 'production') {
+	keys = prod;
+}
+
+export default keys;
+
+// MY skype id: live:.cid.bd63ed933000e56f
