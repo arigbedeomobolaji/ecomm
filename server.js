701d6013d7c4ebd99304c71ac61d35cd882a624a
Arigbede Omobolaji <arigbedeomobolaji@gmail.com>
2021-10-14 17:58:22 +0100

    deploy on heroku

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // configure the environment variables
 import dotenv from 'dotenv';
 dotenv.config();
-
+import path from 'path';
 import express from 'express';
 import mongoose from 'mongoose';
 import userRouter from './router/userRouter.js';
@@ -39,6 +39,15 @@ app.get('/api/config/paystack', (req, res) => {
 app.get('/', (req, res) => {
 	res.send("Welcome Let's code");
 });
+
+// client data
+if (process.env.NODE_ENV === 'production') {
+	// set static folder
+	app.use(express.static('client/build'));
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
 
 // error handling middleware
 app.use((err, req, res, next) => {
